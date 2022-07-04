11c9806db59e37e6c1bf73ca5c5800767e8c0310
Jesse Asamoa <jesseasamoa@gmail.com>
2022-07-04 13:59:08 +0000

    updated settings hosts, static for production

--- abontem/settings.py
+++ abontem/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'abontem.com', 'www.abontem.com', 'https://abontem.com', 'https://www.abontem.com']
 
 
 # Application definition
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # 'django.middleware.security.SecurityMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/home/engavqti/airgadapp/static'
+# STATIC_ROOT = '/home/engavqti/abontemapp/static'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]

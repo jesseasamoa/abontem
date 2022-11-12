92c95189838e3ccfb97d7901fb87c868c51f8560
Jesse Asamoa-Krodua <jesse.asamoa@wacren.net>
2022-11-12 21:33:18 +0000

    Fixing static

--- abontem/settings.py
+++ abontem/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/engavqti/abontemapp/static'
 # STATIC_ROOT = '/static/'
 STATIC_ROOT = '/home/engavqti/abontemapp/static'
 # STATICFILES_DIRS = [

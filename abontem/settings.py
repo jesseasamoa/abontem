b414d8e6eb9315e612af889ee812b494e3f2dd3a
Jesse Asamoa-Krodua <jesse.asamoa@wacren.net>
2022-11-12 16:19:26 +0000

    added i18n languages swahili & french

--- abontem/settings.py
+++ abontem/settings.py
@@ -29,7 +29,7 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u5ecm0a5xb89*-&5q2fo($cjer6mzmscd*h)2mvfh4(ge90h$a'
+SECRET_KEY = '='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
